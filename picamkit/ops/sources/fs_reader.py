dc14ca5c7cfa5a15f028acd7f965bff113f78115
Paul Ryan <bngv8rqg@duck.com>
2024-04-28 15:17:14 +1000

    fs reader: support recursively scanning into subdirectories

--- picamkit/ops/sources/fs_reader.py
+++ picamkit/ops/sources/fs_reader.py
@@ -4,7 +4,7 @@ import cv2
 
 # Mimic the opration of the Pi camera but read images from a directory
 
-def fs_reader(idir, *, sort=False, extensions={'.png', '.jpg', '.jpeg'}):
+def fs_reader(idir, *, sort=False, recursive=False, extensions={'.png', '.jpg', '.jpeg'}):
 
     # make sure the extensions are in the correct format
     exts = list(extensions)
@@ -18,36 +18,50 @@ def fs_reader(idir, *, sort=False, extensions={'.png', '.jpg', '.jpeg'}):
     print(f"Building picamkit.ops.sources.fs_reader")
     print(f"- idir: {idir}")
     print(f"- sort: {sort}")
+    print(f"- recursive: {recursive}")
     print(f"- extensions: {extensions}")
 
+
+    def gen():
+        
+        for idx, item in enumerate(_fs_reader(idir, sort=sort, recursive=recursive, extensions=extensions)):
+            item['idx'] = idx
+            yield item
+
+    return gen()
+
+
+def _fs_reader(idir, *, sort, recursive, extensions={'.png', '.jpg', '.jpeg'}):
+    
     idir = os.path.expanduser(idir)
     images = os.scandir(idir)
     if sort:
         images = list(images)
         images.sort(key=lambda x: x.name)
-
-    def gen():
-        for idx, entry in enumerate(images):
-            if not entry.is_file():
-                continue
-            _, ext = os.path.splitext(entry.name)
-            if not ext in extensions:
-                continue
+    
+    for entry in images:
+        if entry.is_dir() and recursive:
+            yield from _fs_reader(entry.path, sort=sort, recursive=recursive, extensions=extensions)
+            continue
         
-            img = cv2.imread(entry.path, cv2.IMREAD_COLOR)
+        if not entry.is_file():
+            continue
         
-            item = {
-                'idx': idx,
-                'stamp': time.monotonic_ns(),
-                'metadata': {
-                    'name': entry.path,
-                },
-                'main': {
-                    'format': 'RGB888',
-                    'image': img
-                }
+        _, ext = os.path.splitext(entry.name)
+        if not ext in extensions:
+            continue
+    
+        img = cv2.imread(entry.path, cv2.IMREAD_COLOR)
+    
+        item = {
+            'stamp': time.monotonic_ns(),
+            'metadata': {
+                'name': entry.path,
+            },
+            'main': {
+                'format': 'RGB888',
+                'image': img
             }
-            yield item
-
-    return gen()
-
+        }
+        yield item
+    
