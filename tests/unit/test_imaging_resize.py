d4576ed9ace4a379027b6f0bde9b5f52f8f7fd27
Paul Ryan <bngv8rqg@duck.com>
2024-06-21 10:43:19 +1000

    add test for crop

--- tests/unit/test_imaging_resize.py
+++ tests/unit/test_imaging_resize.py
@@ -4,36 +4,21 @@ import picamkit.ops.imaging as imaging
 import pytest
 
 
-@pytest.fixture(params=[(1024, 768, 3), (1920, 1080, 3)])
-def Images(request):
-    shape = (request.param[1], request.param[0], request.param[2])
-    items = []
-    for i in range(4):
-        items.append(
-            {
-                'id': i,
-                'main': {
-                    'image': np.random.randint(0, 255, shape, np.uint8),
-                    'orig_size': request.param
-                }
-            }
-        )
-    return items
-
-
 @pytest.mark.parametrize('width,height', [(640,480), (720,640)])
 def test_ImagesAreResized(Images, width, height):
     pipe = imaging.resize(Images, width=width, height=height)
+    
     for item in pipe:
         img = item['main']['image']
         
-        assert img.shape == (height, width, 3)
+        assert img.shape == (height, width, img.shape[2])
         assert img.dtype == np.uint8
 
 
 @pytest.mark.parametrize('width,height', [(640,480), (720,640)])
 def test_ImagesAreResizedWithAspectPreserved(Images, width, height):
     pipe = imaging.resize(Images, width=width, height=height, preserve_aspect=True, pad=False)
+    
     for item in pipe:
         img = item['main']['image']
         size = item['main']['orig_size']
@@ -47,10 +32,11 @@ def test_ImagesAreResizedWithAspectPreserved(Images, width, height):
 @pytest.mark.parametrize('factor', [(0.25), (0.33333333), (0.5), (0.666666667), (1.0)])
 def test_ImagesAreScaled(Images, factor):
     pipe = imaging.scale(Images, factor=factor)
+    
     for item in pipe:
         img = item['main']['image']
-        size = item['main']['orig_size']
-        shape = (round(size[1]*factor), round(size[0]*factor), size[2])
+        orig_size = item['main']['orig_size']
+        shape = (round(orig_size[1]*factor), round(orig_size[0]*factor), orig_size[2])
         
         assert img.shape == shape
         assert img.dtype == np.uint8
